c71210abff2ca8113b82c937e329b4b26ea0d9de
LuciferMorningstarDev <contact@lucifer-morningstar.xyz>
2022-08-21 16:15:20 +0200

    add tests

---
+++ test/ping.test.js
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) LuciferMorningstarDev <contact@lucifer-morningstar.dev>
+ * Copyright (c) surviv.fun <contact@surviv.fun>
+ * Copyright (C) surviv.fun team and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+'use strict'; // https://www.w3schools.com/js/js_strict.asp
+
+const PING = require('../src');
+
+const checkKeys = (data) => {
+    var hasKeys = true;
+    var needed = ['version', 'players', 'description', 'favicon'];
+    needed.forEach((key) => {
+        if (data[key] == null) hasKeys = false;
+    });
+    return hasKeys;
+};
+
+test('ping', () => {
+    PING.pingUri('minecraft://play.surviv.fun:25565').then((data) => {
+        expect(data).not.toBeNull();
+        expect(checkKeys(data)).toBe(true);
+    });
+});
